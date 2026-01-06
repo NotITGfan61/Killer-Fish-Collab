6af4e0f7598d72a2352d43e775a48652e4a8d998
NotITGfan61 <notitgfan61@gmail.com>
2026-01-06 05:13:07 -0500

    song cut + ending fix

--- lua/nitgfan.lua
+++ lua/nitgfan.lua
@@ -13,13 +13,13 @@ endquad:diffuse(0,0,0,1)
 endtext:zoomto(sw/2,sh/2)
 endtext:y(-sh/4)
 
-set{764,100,'hide'}
+set{700,100,'hide'}
 
-func {764, function()
+func {700, function()
 	endingaf:hidden(0)
 end}
 
-func_ease {765.5, 16, outExpo, 360, 0, 'endingaf:z'}
-func_ease {765.5, 16, outExpo, 1, 0, 'endquad:diffusealpha'}
+func_ease {701.5, 16, outExpo, 360, 0, 'endingaf:z'}
+func_ease {701.5, 16, outExpo, 1, 0, 'endquad:diffusealpha'}
 
-func_ease {781-1, 2, ExpoS, 0, 1, 'endquad:diffusealpha'}
+func_ease {717-1, 2, ExpoS, 0, 1, 'endquad:diffusealpha'}
