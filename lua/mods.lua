5f412b5d53442f3fabf2515b6d462ac087e525ba
PlasticRainbow <aa7652@naver.com>
2026-01-08 23:25:44 +0900

    rough mods sketch on drop 4

--- lua/mods.lua
+++ lua/mods.lua
@@ -54,17 +54,85 @@ require('plastic')
 
 --------------------------------------------------------------------------------------------BG AND SHADER STUFF GOES HERE--------------------------------------------------------------------------------------------------------------------
 
+--conamera (above)
+above.frame:xy(scx, scy)
+above.frame:fov(RealFov(45))
+above.frame:SetFarDist(9E9)
+above.frame:zbuffer(1)
+above.frame:hidden(1)
+	
+local sky_size = 4096*20
+tex_sky:hidden(1)
+	
+above.sky:y(768)
+above.sky:rotationy(90)
+above.sky:zoom( sky_size/640 ):zoomz( sky_size/640 )
+above.sky:cullmode('front')
+above.sky:SetTexture(0, tex_sky:GetTexture())
+	
+above.ocean:xy(0, 768)
+above.ocean:SetWidth(sky_size*2)
+above.ocean:SetHeight(sky_size*2)
+above.ocean:rotationx(90)
+above.ocean:customtexturerect(0, 0, 24, 24)
+	
+above.oceanlight:xy(0, 768)
+above.oceanlight:z(-sky_size)
+above.oceanlight:SetWidth(sky_size)
+above.oceanlight:SetHeight(1600)
+above.oceanlight:diffuse(0.95, 0.95, 0.95, 1)
+above.oceanlight:fadetop(0.4)
+above.oceanlight:fadebottom(0.5)
+above.oceanlight:clearzbuffer(1)
 
+-----------for testing purposes --------------
+func{212, function()
+	above.frame:hidden(0)
+end, persist=true}
+	
+func{276, function()
+	above.frame:hidden(1)
+end, persist=true}
+-----------------------------------------------
+	
+--conamera (underwater)
+under.frame:xy(scx,scy)
+under.frame:hidden(1)
+	
+under.water:zoomto(sw,-sh)
+under.rays:xy(0, 0)
+under.rays:zoomto(sw, sh)
 
 ----------------------------------------------------------------------------------------------DEFINEMOD STUFF GOES HERE----------------------------------------------------------------------------------------------------------------------
 
+--underwater bg definemods
+definemod {'waterfov', 'waterheight', function(fov, ht)
+	under.water:GetShader():uniform1f('fov', fov)
+	under.water:GetShader():uniform1f('waterheight', ht)
+end}
+setdefault{45, 'waterfov', 7, 'waterheight'}
+	
+definemod {'waterrotx', 'waterroty', 'waterrotz', function(rx, ry, rz)
+	under.water:GetShader():uniform1f('rotx', rx)
+    under.water:GetShader():uniform1f('roty', ry)
+	under.water:GetShader():uniform1f('rotz', rz)
+end}
+	
+definemod{'waterposx', 'waterposy', 'waterposz', function(xp, yp, zp)
+	under.water:GetShader():uniform1f('positionx', xp)
+	under.water:GetShader():uniform1f('positiony', yp)
+	under.water:GetShader():uniform1f('positionz', zp)
+end}
 
-
-
+definemod {'sunraypos', function(b)
+    under.rays:GetShader():uniform1f('sunraypos', b)
+end}
+setdefault{80, 'sunraypos'}
 
 ----------------------------------------------------------------------------------------------FUNCTION STUFF GOES HERE-----------------------------------------------------------------------------------------------------------------------
 
 reset{60}
 
+reset{632} -- Reset after Drop 4 (PlasticRainbow)
 
 --------------------------------------------------------------------------------------------------SETUP ENDS HERE----------------------------------------------------------------------------------------------------------------------------
--- lua/plastic.lua
+++ lua/plastic.lua
@@ -1,70 +1,87 @@
 --code goes here here :)
-
---0. BG Stuffs
-	--conamera (above)
-	above.frame:xy(scx, scy)
-	above.frame:fov(RealFov(45))
-	above.frame:SetFarDist(9E9)
-	above.frame:zbuffer(1)
-	above.frame:hidden(1)
+--??. Drop 4 : Arrow Torpedo
+	--ActorProxies for this section
+	drop4.ppframe:xy(scx, scy)
+	drop4.ppframe:fov(RealFov(90))
+	drop4.ppframe:SetFarDist(9E9)
+	drop4.ppframe:zbuffer(1)
+	drop4.ppframe:hidden(1)
 	
-	local sky_size = 4096*20
-	tex_sky:hidden(1)
+	for pn = 1, 2 do
+		PP_D4[pn]:SetTarget(P[pn]:GetChild('NoteField'))
+	end
 	
-	above.sky:y(768)
-	above.sky:rotationy(90)
-	above.sky:zoom( sky_size/640 ):zoomz( sky_size/640 )
-	above.sky:cullmode('front')
-	above.sky:SetTexture(0, tex_sky:GetTexture())
+	definemod{'drop4rotz', function(rz)
+		drop4.ppframe:rotationz(rz)
+	end}
 	
-	above.ocean:xy(0, 768)
-	above.ocean:SetWidth(sky_size*2)
-	above.ocean:SetHeight(sky_size*2)
-	above.ocean:rotationx(90)
-	above.ocean:customtexturerect(0, 0, 24, 24)
-	--above.ocean:hidden(1)
-	
-	above.oceanlight:xy(0, 768)
-	above.oceanlight:z(-sky_size)
-	above.oceanlight:SetWidth(sky_size)
-	above.oceanlight:SetHeight(1600)
-	above.oceanlight:diffuse(0.95, 0.95, 0.95, 1)
-	above.oceanlight:fadetop(0.4)
-	above.oceanlight:fadebottom(0.5)
-	above.oceanlight:clearzbuffer(1)
-	
-	
-	func{212, function()
-		above.frame:hidden(0)
-	end, persist=true}
-	
-	--conamera (underwater)
-	under.frame:xy(scx,scy)
-	under.frame:hidden(1)
-	
-	under.water:zoomto(sw,-sh)
-	
-	definemod {'waterfov', 'waterheight', function(fov, ht)
-        under.water:GetShader():uniform1f('fov', fov)
-		under.water:GetShader():uniform1f('waterheight', ht)
-    end}
-	setdefault{45, 'waterfov', 7, 'waterheight'}
-	
-	 definemod {'waterrotx', 'waterroty', 'waterrotz', function(rx, ry, rz)
-		under.water:GetShader():uniform1f('rotx', rx)
-        under.water:GetShader():uniform1f('roty', ry)
-		under.water:GetShader():uniform1f('rotz', rz)
-    end}
-	
-	under.rays:xy(0, 0)
-	under.rays:zoomto(sw, sh)
-	
-	definemod {'sunraypos', function(b)
-        under.rays:GetShader():uniform1f('sunraypos', b)
-    end}
-	setdefault{80, 'sunraypos'}
-	--[[
-	func{212, function()
+	--happens at underwater??
+	func{568, function()
 		under.frame:hidden(0)
+		
+		for pn = 1, 2 do
+			PP[pn]:hidden(1)
+		end
+		drop4.ppframe:hidden(0)
 	end, persist=true}
-	--]]
+	
+	add{568, 64, linear, -100, 'waterposz', plr = 1}
+	
+	func{632, function()
+		under.frame:hidden(1)
+	end, persist=true}
+	
+	--player hidden stuffs
+	for i = 572, 592, 4 do
+		local pn = 2 - ((i-572)/4)%2
+		
+		P[pn]:SetHiddenRegions( { {i, i+4} } )
+	end
+	
+	for i = 604, 628, 4 do
+		local pn = 2 - ((i-572)/4)%2
+		
+		P[pn]:SetHiddenRegions( { {i, i+4} } )
+	end
+	
+	--torpedo mods??
+	function forbiddenspline(pn, amt)
+		P[pn]:SetXSpline(0,-1,-500*amt,-100,-1)	
+		P[pn]:SetXSpline(1,-1,0,100,-1)
+		P[pn]:SetXSpline(2,-1,-20*amt,400,-1)
+		P[pn]:SetRotZSpline(0,-1,-100*amt,-100,-1)
+		P[pn]:SetRotZSpline(1,-1,20*amt,200,-1)
+		P[pn]:SetRotZSpline(2,-1,0,400,-1)
+		P[pn]:SetZSpline(0,-1,3000*amt,-500,-1)
+		P[pn]:SetZSpline(1,-1,0,500,-1)
+	end
+	
+	set{568, 30, 'flip', 100, 'wave', -512, 'z', 3, 'xmod', 100, 'sudden', 200, 'suddenoffset', 100, 'drawsize', 50, 'movey'}
+	set{568, -90, 'rotationz',  157, 'confusionzoffset', plr = 1}
+	set{568,  90, 'rotationz', -157, 'confusionzoffset', plr = 2}
+	
+	func{568, function()
+		forbiddenspline(1, 1)
+		forbiddenspline(2, 1)
+	end, persist=true}
+	
+	add{568, 28, linear,  360, 'drop4rotz', plr = 1}
+	add{568, 28, linear, -628, 'confusionzoffset'}
+	
+	add{604, 28, linear,  360, 'drop4rotz', plr = 1}
+	add{604, 28, linear, -628, 'confusionzoffset'}
+	
+	--hiding stuffs
+	func{632, function()
+		drop4.ppframe:hidden(1)
+		
+		for pn = 1, 2 do
+			PP[pn]:hidden(0)
+			for c = 0, 3 do
+				P[pn]:ResetXSplines(c)
+				P[pn]:ResetYSplines(c)
+				P[pn]:ResetZSplines(c)
+				P[pn]:ResetRotZSplines(c)
+			end
+		end
+	end, persist=true}
